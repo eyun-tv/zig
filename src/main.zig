ffe800466ce61b6924e7bf87e827724fbf7e5c21
gcxfd <i@rmw.link>
2022-02-18 14:38:41 +0800

    🔶

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,14 @@
 const std = @import("std");
+const network = @import("network");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() anyerror!void {
+    try network.init();
+    defer network.deinit();
+
     try stdout.print("Hello, {s}!\n", .{"world"});
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
-}
+// test "basic test" {
+//    try std.testing.expectEqual(10, 3 + 7);
+// }
